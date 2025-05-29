71af3fc82e67545e5cae874bd6f174871e2ee14a
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-05-30 01:38:32 +0530

    commit

---
+++ Recursion+Backtracking/PrntSubSequence.java
@@ -0,0 +1,13 @@
+public class PrntSubSequence{
+    public static void subSequence(int ind,List<Integer>lst,int[]arr,int len){
+        
+    }
+    public static void main(String[]args){
+        int[]arr = {3,2,1};
+        int len = arr.length;
+        List<Integer> lst = new ArrayList<>();
+        subSequence(0,lst,arr,len);
+    }
+
+
+}

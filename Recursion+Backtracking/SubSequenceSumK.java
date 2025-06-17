3345b639cf2623b5775a2bdffc06325ef79b07d7
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-06-17 16:55:57 +0530

    count + only one sum

--- Recursion+Backtracking/SubSequenceSumK.java
+++ Recursion+Backtracking/SubSequenceSumK.java
@@ -1,22 +1,85 @@
 import java.util.*;
+
+// public class SubSequenceSumK{
+//     public static void pSumK(int ind, int[]arr, List<Integer>lst, int sum , int k){
+// if(sum>k){
+//     return 0;
+// }
+//         if(ind == arr.length){
+//             if(sum == k){
+//                 System.out.println(lst);
+//             }
+//             return ;
+//         }
+//         // include currect el
+//         lst.add(arr[ind]);
+//         pSumK(ind+1,arr,lst,sum+arr[ind],k);
+//         lst.remove(lst.size()-1);
+//         // exclude 
+//         pSumK(ind+1,arr,lst,sum,k);
+//     }
+//     public static void main(String[]args){
+//         int []arr = {1,2,1};
+//         int k = 2;
+//         pSumK(0,arr,new ArrayList<>() , 0,k);
+//     }
+// }
+
+// COUNT THE TOTAL NUMBER OF ANSS
 public class SubSequenceSumK{
-    public static void pSumK(int ind, int[]arr, List<Integer>lst, int sum , int k){
+    public static int pSumK(int ind, int[]arr, int sum , int k){
         if(ind == arr.length){
             if(sum == k){
-                System.out.println(lst);
+                return 1;
             }
-            return ;
+            return 0;
         }
         // include currect el
-        lst.add(arr[ind]);
-        pSumK(ind+1,arr,lst,sum+arr[ind],k);
-        lst.remove(lst.size()-1);
+        int left = pSumK(ind+1,arr,sum+arr[ind],k);
         // exclude 
-        pSumK(ind+1,arr,lst,sum,k);
+        int right = pSumK(ind+1,arr,sum,k);
+
+        int ans = left + right;
+        return ans;
     }
     public static void main(String[]args){
         int []arr = {1,2,1};
         int k = 2;
-        pSumK(0,arr,new ArrayList<>() , 0,k);
+        System.out.println(pSumK(0,arr,0,k));
     }
 }
+
+// TO PRINT ONLY ONE ANS 
+// public class SubSequenceSumK{
+//     public static boolean pSumK(int ind, int[]arr, List<Integer>lst, int sum , int k){
+//         // CONDITION SATISFIED
+//         if(ind == arr.length){
+//             if(sum == k){
+//                 System.out.println(lst);
+//                 return true;
+//             }
+//             else{
+//             return false;
+//             }
+//         }
+//         // include currect el
+//         lst.add(arr[ind]);
+//         if(pSumK(ind+1,arr,lst,sum+arr[ind],k) == true){
+//             return true;
+//         }
+//         lst.remove(lst.size()-1);
+//         // exclude 
+//         if(pSumK(ind+1,arr,lst,sum,k) == true){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     }
+    
+//     public static void main(String[]args){
+//         int []arr = {1,2,1};
+//         int k = 2;
+//         pSumK(0,arr,new ArrayList<>() , 0,k);
+//     }
+// }
